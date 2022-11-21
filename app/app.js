e1adbeda45ae60eba5177a92170727838a533b1d
Brian Kinney <webdevbrian@gmail.com>
2022-11-21 00:49:28 -0500

    Basic UI for importing

--- app/app.js
+++ app/app.js
@@ -1,19 +1,99 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu, MenuItem, ipcMain, dialog} = require('electron')
 const path = require('path')
 const url = require('url')
 
 let window = null
 
+var nodeConsole = require('console');
+var appconsole = new nodeConsole.Console(process.stdout, process.stderr);
+
+ipcMain.on('show-open-dialog', (event, arg)=> {
+
+  const options = {
+    title: 'Open a file',
+    //defaultPath: '/',
+    buttonLabel: 'Import Tacview',
+    filters: [
+      { name: 'CSV', extensions: ['csv'] },
+      { name: 'ACMI', extensions: ['acmi'] }
+    ],
+    message: 'Select a tacview file you would like to import'
+  };
+
+  dialog.showOpenDialog(null, options, (filePaths) => {
+    event.sender.send('open-dialog-paths-selected', filePaths)
+  });
+})
+
+const template = [
+  {
+    label: 'DWR',
+    submenu:[],
+  },
+  {
+     label: 'Edit',
+     submenu: [
+      {
+        role: 'undo'
+      },
+      {
+        role: 'redo'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'cut'
+      },
+      {
+        role: 'copy'
+      },
+      {
+        role: 'paste'
+      }
+    ]
+  },
+  {
+    role: 'window',
+    submenu: [
+      {
+         role: 'minimize'
+      },
+      {
+        role: 'close'
+      }
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'About DCS War Room'
+      }
+    ]
+  }
+]
+
 // Wait until the app is ready
 app.once('ready', () => {
+
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+
   // Create a new window
   window = new BrowserWindow({
-    width: 800,
+    width: 1200,
     height: 600,
+    minWidth: 800,
+    minHeight: 600,
+    center: true,
     backgroundColor: "#D6D8DC",
     // Don't show the window until it's ready, this prevents any white flickering
     show: false
   })
+
+  // Open the dev tools by default
+  window.webContents.openDevTools();
 
   // Load a URL in the window to the local index.html path
   window.loadURL(url.format({
@@ -26,4 +106,11 @@ app.once('ready', () => {
   window.once('ready-to-show', () => {
     window.show()
   })
+
+  appconsole.log('Console Logging test');
+  
+  // Open select file dialog
+  //appconsole.log(dialog.showOpenDialog({ properties: ['openFile', 'multiSelections'] }))
+  //appconsole.log(dialog.showOpenDialog({ properties: ['openFile'] }))
+  
 })
--- app/window.js
+++ app/window.js
@@ -1,12 +1,74 @@
-$(() => {
-  const crypto = require('crypto')
+const { ipcRenderer } = require('electron')
 
-  $('#text-input').bind('input propertychange', function() {
-    const text = this.value
-
-    const md5 = crypto.createHash('md5').update(text, 'utf8').digest('hex')
-    $('#md5-output').text(md5)
-  })
-
-  $('#text-input').focus() // focus input box
+ipcRenderer.on('open-dialog-paths-selected', (event, arg)=> {
+  dialog.handler.outputSelectedPathsFromOpenDialog(arg);
 })
+
+ipcRenderer.on('show-message-box-response', (event, args) => {
+  dialog.handler.outputMessageboxResponse(args);
+})
+
+window.dialog = window.dialog || {},
+
+function(n) {
+
+  dialog.handler = {
+    showOpenDialog: function() {
+      ipcRenderer.send('show-open-dialog');
+    },
+
+    outputSelectedPathsFromOpenDialog: function(paths) {
+      if(paths !== null){
+        const toastLiveExample = document.getElementById('liveToast');
+        const toast = new bootstrap.Toast(toastLiveExample);
+
+        $('.tacview-toast .toast-body').text(paths);
+        toast.show();
+        //alert('user selected: ' + paths);
+      }
+    },
+
+    init: function() {
+      $('#import').click( function () {
+        dialog.handler.showOpenDialog();
+      })
+    }
+  };
+
+  n(function() {
+    dialog.handler.init();
+
+    $('.importing, .no-tacviews').hide();    
+
+    //
+    // Maybe some use in the future: Right click menu 
+    //
+    // const {remote} = require('electron')
+    // const {Menu, MenuItem} = remote
+  
+    // const menu = new Menu()
+  
+    // // Build menu one item at a time, unlike
+    // menu.append(new MenuItem ({
+    //   label: 'MenuItem1',
+    //   click() { 
+    //     console.log('item 1 clicked')
+    //   }
+    // }))
+    
+    // menu.append(new MenuItem({type: 'separator'}))
+    // menu.append(new MenuItem({label: 'MenuItem2', type: 'checkbox', checked: true}))
+    // menu.append(new MenuItem ({
+    //   label: 'MenuItem3',
+    //   click() {
+    //     console.log('item 3 clicked')
+    //   }
+    // }))
+  
+    // // Prevent default action of right click in chromium. Replace with our menu.
+    // window.addEventListener('contextmenu', (e) => {
+    //   e.preventDefault()
+    //   menu.popup(remote.getCurrentWindow())
+    // }, false)
+  })
+}(jQuery);
